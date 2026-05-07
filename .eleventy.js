12297bba247f4bfae343c4c482fa574897d319ac
Francisco Espinoza <282028620+CiscoByte@users.noreply.github.com>
2026-05-07 09:50:40 -0700

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -770,6 +770,7 @@ module.exports = function(eleventyConfig) {
       output: "dist",
       data: `_data`,
     },
+    pathPrefix: "/digitalgarden/",
     templateFormats: ["njk", "md", "11ty.js", "canvas"],
     htmlTemplateEngine: "njk",
     markdownTemplateEngine: false,
